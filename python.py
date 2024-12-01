aeed554f84a7fda529c5c2985ec06b921a95cc69
KARTHIKTHILAK <karthikthilak2010@gmail.com>
2024-12-01 21:25:18 +0530

    added website

---
+++ python.py
@@ -0,0 +1,11 @@
+a = 89
+b = 99
+c = a+b
+print(c)
+
+def change():
+    a = 99
+    b= 777
+    c = a*b
+    print(c)
+change()
---
+++ script.js
@@ -0,0 +1,7 @@
+var main_nav = document.getElementById("main-nav")
+
+function change()
+{
+    main_nav.style.backgroundColor="black"
+    main_nav.style.color="white"
+}
